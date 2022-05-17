3f5f5b714c1565f439f3cb1dbcd504a1088546f7
Malgorzata Kowalik <malgorzata0kowalik@gmail.com>
2022-05-18 01:32:26 +0200

    move calendar script to index.js, add login section

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import Logo from "./images/logo.png"
 import LogoLong from "./images/logo_long.png"
 import closeIcon from "./images/close.png"
 
-// delete -----
-const renderCalendar = require('./calendar').renderCalendar;
-// ------------
-
 const wrapperSearch = document.querySelector('.search-pannel');
 const wrapperOptions = document.querySelector('.options-pannel');
 const wrapperWeather = document.querySelector('.wrapper.weather');
+const wrapperLogin = document.querySelector('.login-pannel');
 
 const searchFieldOrigin = document.querySelector('.search.pannel-field.origin');
 const searchFieldDestination = document.querySelector('.search.pannel-field.destination');
@@ -18,6 +15,7 @@ const searchFieldPassengers = document.querySelector('.search.pannel-field.passe
 
 const originDataField = searchFieldOrigin.querySelector('.origin.data');
 const destinationDataField = searchFieldDestination.querySelector('.destination.data');
+const dateDataField = searchFieldDate.querySelector('.date.data');
 const passengersDataField = searchFieldPassengers.querySelector('.passengers.data');
 
 const optionsFields = [...document.querySelectorAll('.options.pannel-field'), document.querySelector('.calendar')];
@@ -28,27 +26,32 @@ const optionsFieldPassengers = document.querySelector('.options.pannel-field.pas
 
 const optionOrigin = document.querySelector('.option.data.origin');
 const optionsDestination = [...document.querySelectorAll('.option.data.destination')];
+
 const optionsPassengersContainer = document.querySelector('.passengers-container');
 const optionsPassengersSign = [...optionsPassengersContainer.querySelectorAll('.fas')];
 const optionsPassengersAmount = [...optionsPassengersContainer.querySelectorAll('.passenger-amount')];
 const optionsPassengersAcceptBtn = document.querySelector('.accept-passengers');
 
+const btnLogin = document.querySelector('.btn.login');
 const btnAccept = document.querySelector('.btn.accept');
 const btnClose = document.querySelector('.btn.close');
 
+const prevMonthArrow = document.querySelector(".prev")
+const nextMonthArrow = document.querySelector(".next")
+
 let origin;
 let destination;
-let date;
 let adults = 1;
 let children = 0;
 let infants = 0;
+
+const date = new Date();
 
 
 function searchPannelVisibility() {
   if (window.innerWidth <= 800) {
     wrapperSearch.style.display = 'none'
   }
-  console.log()
 }
 function onOption() {
   wrapperOptions.style.display = 'block';
@@ -86,6 +89,93 @@ function onOptionDestination(event) {
   destination = event.target.innerText
   destinationDataField.innerText = destination
   closeOptions()
+}
+
+function renderCalendar() {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  // document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  let dayIdx
+  firstDayIndex === 0 ? dayIdx = 7 : dayIdx = firstDayIndex
+  for (let x = dayIdx; x > 1; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 2}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      (i < new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()) ||
+      (date.getMonth() < new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) ||
+      (date.getFullYear() < new Date().getFullYear())
+    ) {
+      days += `<div class="past-days">${i}</div>`;
+    } else {
+      days += `<div class="possible-days" data-month="${date.getMonth()}" data-year="${date.getFullYear()}" data-day="${i}">${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+  }
+  monthDays.innerHTML = days;
+
+  const optionsDatePossibleDays = [...document.querySelectorAll('.possible-days')];
+  optionsDatePossibleDays.forEach(day => day.addEventListener('click', onOptionDate))
+};
+
+function onOptionDate(event) {
+    let day = event.target.dataset.day
+    let month = event.target.dataset.month
+    let year = event.target.dataset.year
+    dateDataField.innerText = `${day < 10 ? '0' + day : day}/${month < 9 ? '0' + (+month + 1) : (month + 1)}/${year}`
+    const daysDivs = [...document.querySelectorAll('.days>div')];
+    daysDivs.forEach(day => day.classList.remove('chosen'))
+    event.target.classList.add('chosen')
+    closeOptions()
 }
 
 function onOptionPassenger(event) {
@@ -127,6 +217,13 @@ function onOptionPassenger(event) {
       }
     }
   })
+
+  if (children > 0 && infants > 0) {
+    btnAccept.style.marginTop = '0'
+  } else {
+    btnAccept.style.marginTop = '14px'
+  }
+
   let adultsTip = adults > 1 ? 's' : ''
   let childrenTip = children > 1 ? 'ren' : ''
   let infantsTip = infants > 1 ? 's' : ''
@@ -135,17 +232,37 @@ function onOptionPassenger(event) {
 }
 
 function onOptionsClose(event) {
-  if (event.target.closest('.btn.close')) {
-    closeOptions()
+  if (wrapperOptions.style.display == 'block') {
+    if (event.target.closest('.btn.close')) {
+      closeOptions()
+    }
+    if (window.innerWidth <= 800 && !event.target.closest(".main-container")) {
+      closeOptions()
+    }
   }
-  if (window.innerWidth <= 800 && !event.target.closest(".main-container")) {
-    closeOptions()
-  }
+  if (wrapperLogin.style.display == 'block') {
+    if (event.target.closest('.btn.close')) {
+      closeLogin()
+    }
+    if (window.innerWidth <= 800 && !event.target.closest(".main-container") && !event.target.closest(".login")) {
+      closeLogin()
+    }
+  }``
 }
 
 function closeOptions() {
   wrapperOptions.style.display = 'none';
   wrapperSearch.style.display = 'grid'
+}
+
+function closeLogin() {
+  wrapperLogin.style.display = 'none';
+  wrapperSearch.style.display = 'grid'
+}
+
+function onLoginButton() {
+  wrapperLogin.style.display = 'block'
+  searchPannelVisibility();
 }
 
 
@@ -158,3 +275,15 @@ optionOrigin.addEventListener('click', onOptionOrigin)
 optionsDestination.forEach(option => option.addEventListener('click', onOptionDestination))
 optionsPassengersSign.forEach(sign => sign.addEventListener('click', onOptionPassenger))
 optionsPassengersAcceptBtn.addEventListener('click', closeOptions)
+btnLogin.addEventListener('click', onLoginButton)
+
+prevMonthArrow.addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+})
+nextMonthArrow.addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar()
